b9ce30644c5c3fc73ad9e782081acb09d23a58bf
{xenon} <{macolawale@gmail.com}>
2020-10-14 23:27:13 +0100

    Update gradle-wrapper.properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -7,10 +8,41 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              selection(color: Colors.yellow, playNo: 1),
+              selection(color: Colors.red, playNo: 2),
+              selection(color: Colors.blue, playNo: 3),
+              selection(color: Colors.white, playNo: 4),
+              selection(color: Colors.pink, playNo: 5),
+              selection(color: Colors.green, playNo: 6),
+              selection(color: Colors.indigo, playNo: 7),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void playSound(int noteNumber) {
+  final player = AudioCache();
+  player.play('note$noteNumber.wav');
+}
+
+Expanded selection({Color color, int playNo}) {
+  return Expanded(
+    child: Container(
+      color: color,
+      child: FlatButton(
+        onPressed: () {
+          playSound(playNo);
+        },
+        child: null,
+      ),
+    ),
+  );
 }
